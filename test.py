e289ff1efdc74f6da13c836ee4df274afcf52fa2
Haeyoon <chohy0555@gmail.com>
2018-07-28 21:50:12 +0900

    Classification test

--- test.py
+++ test.py
@@ -2,20 +2,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from re import sub
 
+import file_read
+
+features_train, labels_train = file_read.makeData("training")
+features_test, labels_test = file_read.makeData("test")
 
 #clf=KNeighborsClassifier(n_neighbors=3)
 #clf=AdaBoostClassifier(n_estimators=50)
 clf=RandomForestClassifier(n_estimators=100,min_samples_split=5)
 
-'''
-feature_test
-
-labels_test
-
-
 clf.fit(features_train,labels_train)
 pred=clf.predict(features_test)
-print "acc:",accuracy_score(pred,labels_test)
-'''
+print("acc:",accuracy_score(pred,labels_test))
