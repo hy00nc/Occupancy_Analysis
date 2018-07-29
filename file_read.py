ba240193d947586feb060c2d191792dc9a8f0257
gaon-kim <gaon.kim.dev@gmail.com>
2018-07-29 15:27:02 +0900

    makeDataWithoutHumidity 함수 추가

--- file_read.py
+++ file_read.py
@@ -30,3 +30,29 @@ def makeData(mode):
         labels.append(new_list[-1])
 
     return features, labels
+
+def makeDataWithoutHumidity(mode):
+    if mode == "training":
+        f = file_open("datatraining.txt")
+    elif mode == "test":
+        f = file_open("datatest.txt")
+
+    lines = f.readlines()
+
+    features = list()
+    labels = list()
+
+    is_first_line = True
+    for line in lines:
+        if is_first_line:
+            is_first_line = False
+            continue
+        splitted_line = line.split(',')[3:]
+        splitted_line[-1] = sub("\n", "", splitted_line[-1])
+        new_list = list()
+        for attribute in splitted_line:
+            new_list.append(float(attribute))
+        features.append(new_list[0:-1])
+        labels.append(new_list[-1])
+
+    return features, labels
